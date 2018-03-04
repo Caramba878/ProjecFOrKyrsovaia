86b7cf26d95e94d462d2a2e618088ff5203c6ad8
Volunter <homerjsimpsons1998@gmail.com>
2018-03-04 15:39:09 +0300

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -105,7 +105,7 @@ $sql_select = "SELECT * FROM Klient";
 $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll(); 
 if(count($registrants) > 0) {
-    echo "<h2>KLIENT</h2>";
+    echo "<br><h2>KLIENT</h2>";
     echo "<table>";
     echo "<tr><th>Name</th>";
     echo "<th>SecondName</th>";
