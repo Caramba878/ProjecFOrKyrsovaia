883b6e2bb873b4c6a87df07c775818a80535f387
Volunter <homerjsimpsons1998@gmail.com>
2018-03-04 15:16:08 +0300

    Update CreateKlient.php

--- CreateKlient.php
+++ CreateKlient.php
@@ -3,12 +3,12 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:vol2.database.windows.net,1433; Database = BD", "Volun", "Simpsons1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "CREATE TABLE Klient (
-	id INT(30) NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL IDENTITY(1,1)
 	Name varchar(30) NOT NULL,
 	SecondName varchar(30) NOT NULL,
 	Phone TEXT NOT NULL,
 	PRIMARY KEY (`id`)
-);";
+)";
     $conn->query($sql);
 }
 catch (PDOException $e) {
