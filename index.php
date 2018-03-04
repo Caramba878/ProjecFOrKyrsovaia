fdfe91a93f17a141017a5e9141acb880159cf839
Volunter <homerjsimpsons1998@gmail.com>
2018-03-04 19:56:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start(); // каждый файл, в котором Вы хотите использовать данные сессий должен в начале кода содержать команду "запуска сессии" 
+?> 
 <link rel="stylesheet" href="http://bootstraptema.ru/plugins/2015/bootstrap3/bootstrap.min.css" />
 <link rel="stylesheet" href="http://bootstraptema.ru/plugins/font-awesome/4-4-0/font-awesome.min.css" />
 <script src="http://bootstraptema.ru/plugins/jquery/jquery-1.11.3.min.js"></script>
@@ -72,7 +75,7 @@ body{background:#2c3338;}
  <div class="panel-body">
  <div class="text-center" id="author">
 
- <h3>Логин</h3>
+ <h3><?php echo ".$_SESSION['log'].";   ?></h3>
  <small class="label label-warning">Российская Федерация</small>
 
  </div>
