fac3ca934a701d1f3d76ca3ebec6bad65abb9646
Volunter <homerjsimpsons1998@gmail.com>
2018-03-04 13:05:20 +0300

    Update CreateEnter.php

--- CreateEnter.php
+++ CreateEnter.php
@@ -2,13 +2,7 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:vol2.database.windows.net,1433; Database = BD", "Volun", "Simpsons1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-       $sql = "CREATE TABLE Enter (
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    Login VARCHAR(30) NOT NULL,
-    Password VARCHAR(30) NOT NULL,
-    Number TEXT NOT NULL	
-)";
+       $sql = "DROP TABLE Enter";
     $conn->query($sql);
 }
 catch (PDOException $e) {
