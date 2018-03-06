f955233429db1c717146eb0ebdfd0d7ae26c8e83
Volunter <homerjsimpsons1998@gmail.com>
2018-03-06 17:02:15 +0300

    Update autorization.php

--- autorization.php
+++ autorization.php
@@ -32,6 +32,23 @@ $sql_select = "SELECT id FROM Enter where (Login = '$log' And Password = '$pass'
  $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll(); 
  
+	
+	     if(count($registrants) > 0) {
+    echo "<h2>People who are authorization:</h2>";
+    echo "<table>";
+    echo "<tr><th>id</th></tr>";
+	      foreach($registrants as $registrant){
+		      $_SESSION['login'] =$registrant['Login'];
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: index.php');
+        	
+}
+}	        echo "</table>";
+ else {
+    echo "<h3>Incorrect input data.</h3>";
+}
+	
+	
     
 }
 catch (PDOException $e) {
