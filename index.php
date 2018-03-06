c672ea0fcc35f0e93b4a9e3597bf27630abce87f
Volunter <homerjsimpsons1998@gmail.com>
2018-03-06 15:45:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
+<?php  
+if (!isset($_SESSION['login'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
 
+?>
 <link rel="stylesheet" href="http://bootstraptema.ru/plugins/2015/bootstrap3/bootstrap.min.css" />
 <link rel="stylesheet" href="http://bootstraptema.ru/plugins/font-awesome/4-4-0/font-awesome.min.css" />
 <script src="http://bootstraptema.ru/plugins/jquery/jquery-1.11.3.min.js"></script>
