405c238819d8ac19f1f1d450a1228dac565a72c5
Volunter <homerjsimpsons1998@gmail.com>
2018-03-07 15:46:08 +0300

    Update test.php

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     
     
-    $sql_select1 = "Select Name From Klient Join Enter On Klient.id = Enter.id Where Login = 'AAA'"
+    $sql_select1 = "Select Name From Klient Join Enter On Klient.id = Enter.id Where Login = 'AAA'";
  	//$n = $conn->query($sql_select1);
 		//$row = $n->fetchAll();
     //echo var_dump($row);
