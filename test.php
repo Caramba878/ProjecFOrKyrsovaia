4ac92908c4037a476f868fa4802b8445397abbd3
Volunter <homerjsimpsons1998@gmail.com>
2018-03-07 15:39:10 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,18 @@
 <?php
-var_dump($row)
+
 try {
     $conn = new PDO("sqlsrv:server = tcp:vol2.database.windows.net,1433; Database = BD", "Volun", "Simpsons1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    
+    
+    $sql_select1 = "Select Name From Klient Join Enter On Klient.id = Enter.id Where Login = '$log'"
+ 	$n = $conn->query($sql_select1);
+		$row = $n->fetchAll()
+    echo var_dump($row);
+    
+    
+    
+    
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
