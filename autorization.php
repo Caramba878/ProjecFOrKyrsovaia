386e143ae85d50f11a5b0cfba1726574e9ac1caa
Volunter <homerjsimpsons1998@gmail.com>
2018-03-07 14:57:28 +0300

    Update autorization.php

--- autorization.php
+++ autorization.php
@@ -42,8 +42,7 @@ $sql_select = "SELECT * FROM Enter where (Login = '$log' And Password = '$pass')
 	$sql_select1 = "Select Name From Klient Join Enter On Klient.id = Enter.id Where Login = '$log'"
  	$n = $conn->query($sql_select1);
 		foreach ($n as $row) {
-		$_SESSION['name'] = $row["Name"];
-		
+		$_SESSION['name'] = $row['Name'];
 		$_SESSION['secondName'] = "123";
 		  $_SESSION['login'] = $log;
   	  $_SESSION['success'] = "You are now logged in";
