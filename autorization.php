678d78bf6a9f2bede3288053bb6bc7c2d8023f6b
Volunter <homerjsimpsons1998@gmail.com>
2018-03-07 16:06:26 +0300

    Update autorization.php

--- autorization.php
+++ autorization.php
@@ -39,11 +39,14 @@ if (isset($_POST['submit'])) {
 
 $sql_select = "SELECT * FROM Enter where (Login = '$log' And Password = '$pass')";
  $stmt = $conn->query($sql_select);
-		
-			$sql_select1 = "Select Name, secondName From Klient Join Enter On Klient.id = Enter.id Where Login = '$log'";
- 	$n = $conn->query($sql_select1);
+
 	
 	if ($stmt->fetchColumn() > 0){
+		
+		
+				
+			$sql_select1 = "Select Name, secondName From Klient Join Enter On Klient.id = Enter.id Where Login = '$log'";
+ 	$n = $conn->query($sql_select1);
 		
 		    foreach ($n as $row) {
 		session_start();
