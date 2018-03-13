f35f66af42467417608482e91252ed8a9259d46c
Volunter <homerjsimpsons1998@gmail.com>
2018-03-13 16:09:14 +0300

    Update CreateKlient.php

--- CreateKlient.php
+++ CreateKlient.php
@@ -4,10 +4,10 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "CREATE TABLE Klient (
 	id INT NOT NULL IDENTITY(1,1)
-	PRIMARY KEY (id),
 	Name varchar(30),
 	SecondName varchar(30),
-	Phone VARCHAR(11)
+	Phone TEXT NOT NULL,
+	PRIMARY KEY (id)
 )";
     $conn->query($sql);
 }
