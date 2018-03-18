a3f52fa145f08ff4a0d5aae9abce96913340d34c
Volunter <homerjsimpsons1998@gmail.com>
2018-03-18 18:07:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -21,16 +21,6 @@ if (isset($_GET['logout'])) {
   }
 
 
-
-	  
-	   $sql_select2 = "Select Ncard From Card Join Klient On Card.Phone = Klient.Phone Where Phone = 899999999990";
- 	$k = $conn->query($sql_select2);
-    foreach ($k as $row) {
-	$ncard = $row['Ncard'];
-	}
-	   
-
-
 ?>
 <link rel="stylesheet" href="http://bootstraptema.ru/plugins/2015/bootstrap3/bootstrap.min.css" />
 <link rel="stylesheet" href="http://bootstraptema.ru/plugins/font-awesome/4-4-0/font-awesome.min.css" />
@@ -136,7 +126,17 @@ body{background:#2c3338;}
    <label>Ваша карта</label>
    <font color = "black">
   <select>
-   <option><?php  echo $ncard; ?></option> 
+   <option><?php
+	   
+	   
+	   
+	      $sql_select2 = "Select Ncard From Card Join Klient On Card.Phone = Klient.Phone Where Phone = 899999999990";
+ 	$k = $conn->query($sql_select2);
+    foreach ($k as $row) {
+	$ncard = $row['Ncard'];
+	}
+	   
+	 echo $ncard; ?></option> 
 </select> </font>
 
 
