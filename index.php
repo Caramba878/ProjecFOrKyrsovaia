94b236f236e251ef65c3e4d4b8db21306e845586
Volunter <homerjsimpsons1998@gmail.com>
2018-03-18 17:29:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     
     
-    $sql_select2 = "Select Ncard From Ncard Join Klient On Klient.Phone = Enter.Phone Where Phone = '$_SESSION['phone']'";
+    $sql_select2 = "Select Ncard From Card Join Klient On Klient.Phone = Enter.Phone Where Phone = '$_SESSION['phone']'";
  	$k = $conn->query($sql_select2);
     foreach ($k as $row) {
 	$ncard = $row['Ncard'];
