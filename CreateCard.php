36bafce2d9015f5104040c09b9a36f62000c2644
Volunter <homerjsimpsons1998@gmail.com>
2018-03-18 09:23:21 +0300

    Create CreateCard.php

---
+++ CreateCard.php
@@ -0,0 +1,18 @@
+<?php
+try {
+    $conn = new PDO("sqlsrv:server = tcp:vol2.database.windows.net,1433; Database = BD", "Volun", "Simpsons1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+  $sql = "CREATE TABLE Card (
+	Ncard varchar(18) NOT NULL,
+	Balance INT(30) NOT NULL,
+	Phone varchar(11) NOT NULL,
+	PRIMARY KEY (`Ncard`)
+)";
+    $conn->query($sql);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+?>
