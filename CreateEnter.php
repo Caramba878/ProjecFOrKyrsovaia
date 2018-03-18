d6b256ffd2bc041d4a4ecd586ad933aa905de95c
Volunter <homerjsimpsons1998@gmail.com>
2018-03-18 08:28:44 +0300

    Update CreateEnter.php

--- CreateEnter.php
+++ CreateEnter.php
@@ -3,8 +3,7 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:vol2.database.windows.net,1433; Database = BD", "Volun", "Simpsons1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "CREATE TABLE Enter (
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
+    PRIMARY KEY(Login),
     Login VARCHAR(30),
     Password VARCHAR(30)
 )";
