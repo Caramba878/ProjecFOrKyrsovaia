7b6baa92e2b852a33d77ceda2a906b228bc6f780
Volunter <homerjsimpsons1998@gmail.com>
2018-03-18 14:33:29 +0300

    Create DropCard.php

---
+++ DropCard.php
@@ -0,0 +1,12 @@
+<?php
+try {
+    $conn = new PDO("sqlsrv:server = tcp:vol2.database.windows.net,1433; Database = BD", "Volun", "Simpsons1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "DROP TABLE Card";
+    $conn->query($sql);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+?>
