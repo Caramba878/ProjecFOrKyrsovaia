0123fa8431908b85a702704c4ecdc7be19660c84
Volunter <homerjsimpsons1998@gmail.com>
2018-03-19 13:31:39 +0300

    Update autorization.php

--- autorization.php
+++ autorization.php
@@ -43,28 +43,25 @@ $sql_select = "SELECT * FROM Enter where (Login = '$log' And Password = '$pass')
 	
 	if ($stmt->fetchColumn() > 0){
 		
+		session_start();
 				
 		$sql_select1 = "Select Name, SecondName, Phone From Klient Join Enter On Klient.id = Enter.id Where Login = '$log'";
  	$n = $conn->query($sql_select1);
 	   
 
-			session_start();
+			
 		    foreach ($n as $row) {
 		$_SESSION['name'] = $row["Name"];
 		$_SESSION['secondName'] = $row["SecondName"];
 		  $_SESSION['login'] = $log;
 			    $phone = $row["Phone"];
+		    }
 		 $sql_select2 = "Select Ncard From Card Join Klient On Card.id = Klient.id Where Phone ='$phone'";
  		$k = $conn->query($sql_select2);
-			    
-			    
-	
     		    foreach ($k as $row1) {
 		 $_SESSION['ncard']= $row1['Ncard'];
-					}
-		    
-	
-		}
+			}
+		
 		
 				    
   	  $_SESSION['success'] = "You are now logged in";
