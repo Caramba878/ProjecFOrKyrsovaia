cf31b80b65fd99d979f4ceda82d18b35fd4f7862
Volunter <homerjsimpsons1998@gmail.com>
2018-03-20 10:38:51 +0300

    Update CreateCard.php

--- CreateCard.php
+++ CreateCard.php
@@ -4,6 +4,7 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
   $sql = "CREATE TABLE Card (
+  	id INT NOT NULL IDENTITY(1,1)
 	Ncard varchar(30) NOT NULL,
 	Balance INT NOT NULL,
 	Phone varchar(30) NOT NULL,
