346dfe9d3386a9d653cd83027ab3b14b2f83c3bc
Volunter <homerjsimpsons1998@gmail.com>
2018-03-25 09:53:50 +0300

    Update CreateCard.php

--- CreateCard.php
+++ CreateCard.php
@@ -4,17 +4,14 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
   $sql = "CREATE TABLE Card (
-  	id INT NOT NULL IDENTITY(1,1)
+  	id INT NOT NULL IDENTITY(1,1) FOREIGN KEY REFERENCES Klient(id)
 	PRIMARY KEY (Ncard),
 	Ncard varchar(30) NOT NULL,
 	Balance INT NOT NULL,
 	Phone varchar(30) NOT NULL
 	
 )";
-	
-	$sql1 = "ALTER TABLE Card ADD CONSTRAINT Card_fk0 FOREIGN KEY (id) REFERENCES Klient(id)";	
     $conn->query($sql);
-	 $conn->query($sql1);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
