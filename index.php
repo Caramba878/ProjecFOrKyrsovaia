9fe2f24f416e7a5a618bf716bf1f299ebcb38c9e
Volunter <homerjsimpsons1998@gmail.com>
2018-03-29 19:07:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -185,48 +185,10 @@ body{background:#2c3338;}
  }
  
  if(isset($_POST["submit"])) {
- 	$sum = $_POST['sum'];
- 	$card = $_POST['card'];
- 	$balance2;
  	
- 	
- 	$sql_select = "SELECT * FROM Card WHERE Ncard LIKE '%".$card."%'";
- $stmt = $conn->query($sql_select);
- $reg = $stmt->fetchAll(); 
- 
- 	if(count($reg) == 0) {
-     echo "<h2>This Ncard doesn't exist</h2>";
-     }
- 	else
- 	{
- 		
- 		$balance1 = $balance - $sum;
- 		$sql_in = 
- "Update Card Set Balance = '$balance1' Where Ncard = '$n' ";
- 		$stmt = $conn->prepare($sql_in);
-     		$stmt->execute();
- 		
- 		
- 		$sql_select3 = "Select Balance From Card Where Ncard ='$card'";
-  	$k = $conn->query($sql_select3);
- 		$data = $k->fetchAll();
-     foreach($data as $registrant) {
- 	     $balance2 = $registrant['Balance'];	   
-     } 
- 		
- 		
- 	$balance3 =$balance2 +$sum;
- 		$sql_in = 
- "Update Card Set Balance = '$balance3' Where Ncard = '$card' ";
- 		$stmt = $conn->prepare($sql_in);
-     		$stmt->execute();
- 		
  		
  		echo "<h2>Operation is done</h2>";
  		
- 	
- }
- 	
  	
  	
  	
