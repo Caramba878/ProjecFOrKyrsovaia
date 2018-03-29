ae60b048889ddfb8c86d107d9bd9ea462f6e21ab
Volunter <homerjsimpsons1998@gmail.com>
2018-03-29 16:53:25 +0300

    Update InsertKlient.php

--- InsertKlient.php
+++ InsertKlient.php
@@ -14,6 +14,17 @@ $sql_in =
     $stmt->bindValue(2, "Klientovich".$i);
 	  $stmt->bindValue(3, "8999999999".$i);
     $stmt->execute();
+		
+	$sql_inE = 
+"INSERT INTO Enter (Login,Password) 
+                   VALUES (?,?,?)";
+    $stmt = $conn->prepare($sql_in);
+    $stmt->bindValue(1, "Login".$i);
+    $stmt->bindValue(2, "Pas".$i);
+    $stmt->execute();	
+		
+		
+		
 	}
     }
 catch (PDOException $e) {
