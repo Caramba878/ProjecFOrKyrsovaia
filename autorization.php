7c1655652c583b327c73e8f6919ebc8a214c81d7
Volunter <homerjsimpsons1998@gmail.com>
2018-03-29 17:20:42 +0300

    Update autorization.php

--- autorization.php
+++ autorization.php
@@ -73,7 +73,7 @@ $sql_select = "SELECT * FROM Enter where (Login = '$log' And Password = '$pass')
 
 
   
-			    $sql_select2 = "Select Phone From Card Join Klient On Card.id = Klient.id Where Name = 'Klient0'";
+			    $sql_select2 = "Select Phone From Klient Where Name = 'Klient0'";
  	$k = $conn->query($sql_select2);
 		$data = $k->fetchAll();
 		if(count($data) > 0) {
