ab37dfd1b275034e8e5066b69ffb150a4e5f2846
Volunter <homerjsimpsons1998@gmail.com>
2018-03-30 17:29:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -174,15 +174,6 @@ body{background:#2c3338;}
 	 
 	
 <?php
-  try {
-     $conn = new PDO("sqlsrv:server = tcp:vol2.database.windows.net,1433; Database = BD", "Volun", "Simpsons1");
-     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
- }
- catch (PDOException $e) {
-     print("Error connecting to SQL Server.");
-     die(print_r($e));
- }
  
  if(isset($_POST["submit"])) {
  	$sum = $_POST['sum'];
