b10fa5d51dd6f3fcbf17665cc4019d1c09071f85
Volunter <homerjsimpsons1998@gmail.com>
2018-03-31 12:57:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -191,7 +191,7 @@ try {
 	$operation = $_POST['operation'];
  	$sum = $_POST['sum'];
  	$card = $_POST['card'];
-	$date = date("F j, Y, g:i a");
+	$date = date("Y-m-d H:i:s");
  	$balance2;
 	
 	 if($operation == 2){
